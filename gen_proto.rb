6d288a007b493e9ca105b6b63267bc53bd12b04f
Glenn <hazelnutvt04@gmail.com>
2019-02-01 22:35:11 -0500

    Moved template files to a templates directory

--- gen_proto.rb
+++ gen_proto.rb
@@ -3,9 +3,14 @@
 
 class Program
    attr_accessor :basename
-  
-   def initialize(basename)
+   attr_accessor :classname
+   attr_accessor :header_defines
+
+   def initialize(filename)
+      basename = filename.split('.').first
       @basename = basename
+      @classname = basename.slice(0,1).capitalize + basename.slice(1..-1)
+      @header_defines = '_' + basename.gsub('.','_').upcase + '_H_'
    end
 
    # Expose private binding() method 
@@ -16,12 +21,16 @@ end
 
 def gen_proto
    valid_suffixes = [
-      "rb",
+      "class_cpp",
+      "class_h",
       "cpp",
+      "rb"
    ]
    templates = [
-      "rb.erb",
+      "class_cpp.erb",
+      "class_h.erb",
       "cpp.erb",
+      "rb.erb"
    ]
  
    zip = valid_suffixes.zip(templates)
@@ -37,15 +46,14 @@ def gen_proto
    end
    puts "filename is #{filename}" if debug
 
-   tsuffix = filename.split('.')[1]
-   tprefix = filename.split('.')[0]
+   tsuffix = filename.split('.').last
 
    if tsuffix
-      tfilename = ziphash["#{tsuffix}"]
+      tfilename = "templates/" + ziphash["#{tsuffix}"]
       if tfilename
          puts "Template filename is #{tfilename}" if debug
 
-         program = Program.new(tprefix)
+         program = Program.new(filename)
          renderer = ERB.new(File.read(tfilename))
          puts output = renderer.result(program.get_binding)
 
